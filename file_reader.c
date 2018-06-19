9ff7cc7df95e146861c1cebb472cce7853f33a38
Dylan Corbett <dylancorbett@icloud.com>
2018-06-18 21:52:24 -0700

    Make simple file reader

---
+++ file_reader.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+
+	if (argc != 2) {
+
+		// Explain how program should execute
+		printf("Usage: %s <filename>", argv[0]);
+
+	} else {
+
+		// Argv[1] is meant to be filename
+		FILE *file = fopen(arg[1], "r");
+
+		if (file == 0) {
+
+			printf("Could not open file\n");
+
+		} else {
+
+			int x;
+
+			while ((x = fgetc(file)) != EOF) {
+
+				printf("%c", x);
+
+			}
+
+			fclose(file);
+
+		}
+	}
+}
+
