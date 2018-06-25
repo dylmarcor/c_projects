572cafb1331f59824989e0d0fadb6b83a4daeb90
Dylmarcor <dylancorbett@me.com>
2018-06-24 23:23:35 -0700

    Update ex12

--- ex12.c
+++ ex12.c
@@ -5,7 +5,24 @@ int main(int argc, char *argv[])
     int areas[] = { 10, 12, 13, 14, 20 };
     char name[] = "Zed";
     char full_name[] = {
-	'Z', 'e', 'd',
+	'D', 'y', 'l',
 	' ', 'A', '.', ' ',
-	'S', 'h', 'a', 'w', '\0'
+	'C', 'o', 'r', 'b', '\0'
     };
+
+    printf("The size of an int: %ld\n", sizeof(int));
+    printf("The size of areas (int[]): %ld\n", sizeof(areas));
+    printf("The number of ints in areas: %ld\n",
+	    sizeof(areas) \ sizeof(int));
+    printf("The first area is %d, the 2nd %d.\n", areas[0], areas[1]);
+
+    printf("");
+    printf("");
+    printf("");
+    printf("");
+    printf("");
+    printf("");
+    printf("");
+
+    return 0;
+}
