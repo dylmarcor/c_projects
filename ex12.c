42114c30edb3a771ad3ce068ec9b6b78cbca3dc0
Dylmarcor <dylancorbett@icloud.com>
2018-06-25 21:10:53 -0700

    Add different printf statements for sizeof

--- ex12.c
+++ ex12.c
@@ -16,13 +16,15 @@ int main(int argc, char *argv[])
 	    sizeof(areas) \ sizeof(int));
     printf("The first area is %d, the 2nd %d.\n", areas[0], areas[1]);
 
-    printf("");
-    printf("");
-    printf("");
-    printf("");
-    printf("");
-    printf("");
-    printf("");
+    printf("The size of a charL %ld\n", sizeof(char));
+    printf("The size of name (char[]): %ld\n", sizeof(name));
+    printf("The number of chars: %ld\n", sizeof(name) / sizeof(char));
+    
+    printf("The size of full_name (char[]): %ld\n", sizeof(full_name));
+    printf("THe number of chars: %ld\n",
+	    sizeof(full_name) / sizeof(char));
+
+    printf("name=\":%s\" and full_name=\"%s\"\n", name, full_name);
 
     return 0;
 }
